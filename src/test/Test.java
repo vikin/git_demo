87837466377928a754e8e6333dfe09b9564bbfe1
vikin <vikin3@163.com>
2013-12-07 20:15:24 +0800

    添加测试

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,7 +1,20 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello test!");
+		
+		List<String> list = new ArrayList<String>();
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		list.add("d");
+		
+		for(String item:list){
+			System.out.println(item);
+		}
 	}
 }
