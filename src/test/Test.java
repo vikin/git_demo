63279d086b2a775231a5eae8b8c6fd10bd357c75
vikin <vikin3@163.com>
2013-12-07 20:31:45 +0800

    update

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,6 +4,6 @@ package test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello test!");
-		System.out.println("My first git code!");
+		System.out.println("My first git code1111!");
 	}
 }
